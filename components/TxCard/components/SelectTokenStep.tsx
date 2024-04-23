f56b3c63362072649bdd9e79455638ee87efca54
Santiago Giaccobasso <santigiacco@Santiagos-MacBook-Pro.local>
2024-04-23 17:21:01 -0300

    select token styles and validation

--- components/TxCard/components/SelectTokenStep.tsx
+++ components/TxCard/components/SelectTokenStep.tsx
@@ -26,8 +26,20 @@ const SelectTokenStep: React.FC<SelectTokenStepProps> = ({ onClickAction }) => {
       <motion.div className="justify-center w-full flex text-xl text-blue-500">
         SELECT TOKEN
       </motion.div>
-      <label htmlFor="amount" className="mt-5 block font-medium text-white">
-        Token ID:
+      <label htmlFor="amount" className="mt-8 block font-medium text-white">
+        <motion.div className="flex justify-between w-full">
+          <motion.p>Token ID:</motion.p>
+
+          <AnimatePresence>
+            <motion.div className="h-6">
+              {name && symbol && (
+                <motion.p key="token-info-content" className="text-green-400">
+                  {`${name} (${symbol})`}
+                </motion.p>
+              )}
+            </motion.div>
+          </AnimatePresence>
+        </motion.div>
       </label>
       <textarea
         value={tokenId}
@@ -36,21 +48,11 @@ const SelectTokenStep: React.FC<SelectTokenStepProps> = ({ onClickAction }) => {
         placeholder="Enter Interchain Token ID"
         autoCorrect="off"
         spellCheck="false"
-        className="mt-2 h-24 text-right font-medium text-md text-white w-full bg-gray-900 border border-gray-700 rounded-md py-2 px-4 focus:outline-none focus:border-blue-500"
+        className={`my-2 h-24 text-right font-medium text-md text-white w-full bg-gray-900 border ${
+          name ? "border-green-500" : "border-gray-700"
+        } rounded-md py-2 px-4 focus:outline-none focus:border-blue-500`}
         style={{ resize: "none" }}
       />
-      <AnimatePresence>
-        <motion.p key="token-info-title" className={selectedTokenTitleClasses}>
-          Selected Token:{symbol ? " " : " none"}
-        </motion.p>
-        <motion.div className="h-6">
-          {name && symbol && (
-            <motion.p key="token-info-content" className="text-green-400">
-              {`${name} (${symbol})`}
-            </motion.p>
-          )}
-        </motion.div>
-      </AnimatePresence>
 
       <motion.div className="mt-4 flex w-full justify-end">
         <LoadingButton
